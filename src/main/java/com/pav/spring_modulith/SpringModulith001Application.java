7231b1473ec101a007b283ff33a5b9967bbaf610
Prajesh Ananthan V <prajesh.ananthan@outlook.com>
2025-11-23 16:07:43 +0800

    Initial commit

---
+++ src/main/java/com/pav/spring_modulith/SpringModulith001Application.java
@@ -0,0 +1,13 @@
+package com.pav.spring_modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulith001Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringModulith001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pav/spring_modulith/SpringModulith001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pav.spring_modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulith001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
