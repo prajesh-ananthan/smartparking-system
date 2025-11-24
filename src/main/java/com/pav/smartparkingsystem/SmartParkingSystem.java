ebe2ea2381ef0f7bd4eabb664f01d2dc5c407671
Prajesh V Ananthan <prajesh.ananthan@outlook.com>
2025-11-24 09:01:17 +0800

    Renamed file

--- src/main/java/com/pav/smartparkingsystem/SmartParkingSystem.java
+++ src/main/java/com/pav/smartparkingsystem/SmartParkingSystem.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringModulith001Application {
+public class SmartParkingSystem {
 
 	static void main(String[] args) {
-		SpringApplication.run(SpringModulith001Application.class, args);
+		SpringApplication.run(SmartParkingSystem.class, args);
 	}
 
 }
